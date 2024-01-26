81dbb3f6502f5c7139554240c275347fc3348dcd
Andrei <andr1mpn@gmail.com>
2024-01-26 10:58:22 +0300

    New

--- training_LSTM copy.py
+++ training_LSTM copy.py
@@ -180,11 +180,11 @@ class CustomDataset(Dataset):
     
 
 dropout_list = [0.1]
-optimizer_list = [ "Adam"]
-learning_rate_list = [0.005]
-batch_size_list = [1000]
-epoch_list = [200]
-hidden_dim_list = [96, 128, 256]
+optimizer_list = ["Adam", "SGD", "RMSprop"]
+learning_rate_list = [0.0001, 0.01, 0.1, 1]
+batch_size_list = [500, 1000, 5000]
+epoch_list = [50, 100, 200]
+hidden_dim_list = [96, 128, 192, 256]
 activation_function_list = ["Linear", "ReLU", "Leaky ReLU", "Sigmoid", "Tanh"]
 random.shuffle(dropout_list)
 random.shuffle(optimizer_list)
